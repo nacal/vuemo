79e0e68048aa66f12d206c19cad1d56549da7a6b
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-10-07 12:09:24 +0900

    add vuemo

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="container">
-    <h1 class="font-bold text-3xl">Nuxt Tailwind Templete</h1>
-    <nuxt-link to="/_tailwind/" class="underline mt-4"
-      >tailwind-config-viewer</nuxt-link
-    >
-  </div>
+  <main class="container">
+    <button ref="vuemoElement" type="button" @click="replay">Burst!</button>
+  </main>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      burst: null,
+    }
+  },
+  mounted() {
+    this.burst = this.$vuemo.Burst({
+      parent: this.$refs.vuemoElement,
+      radius: { 25: 75 },
+      count: 10,
+      duration: 2000,
+      children: {
+        shape: ['circle', 'polygon'],
+        fill: ['#11CDC5', '#FC2D79', '#F9DD5E'],
+        angle: { 0: 180 },
+        degreeShift: 'rand(-360, 360)',
+        delay: 'stagger(0, 25)',
+      },
+    })
+  },
+  methods: {
+    replay() {
+      this.burst.replay()
+    },
+  },
+}
 </script>
 
 <style lang="postcss">
---
+++ plugins/vuemo.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Vuemo from '@azukisiromochi/vue-mo.js'
+
+// Vue.component('vue-mo', Vuemo)
+Vue.use(Vuemo)
